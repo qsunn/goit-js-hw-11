7ce6758b3e75b370379a5f4ff49b1d11b33f6fde
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-09-13 20:23:14 +0300

    upg

---
+++ src/js/fetchImages.js
@@ -0,0 +1,28 @@
+import Notiflix from 'notiflix';
+
+const axios = require('axios');
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '7130529-cd9ea3f018b85a189f3c85b8b';
+
+export async function fetchImages(input, page) {
+    try {
+        const response = await axios.get(BASE_URL, {
+            params: {
+                key: API_KEY,
+                q: input,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                page: page,
+                per_page: 40
+            }
+        });
+        const data = response.data;
+        if (data.totalHits === 0) throw new Error();
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        return data;
+    } catch {
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    };
+};
---
+++ src/js/index.js
@@ -0,0 +1,70 @@
+import Notiflix from 'notiflix';
+import { fetchImages } from './fetchImages';
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
+
+let page = 1;
+let input = '';
+
+const generateCard = (card) => {
+    return `
+        <div class="photo-card">
+            <img src="${card.webformatURL}" alt="${card.tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes</b> ${card.likes}
+                </p>
+                <p class="info-item">
+                    <b>Views</b> ${card.views}
+                </p>
+                <p class="info-item">
+                    <b>Comments</b> ${card.comments}
+                </p>
+                <p class="info-item">
+                    <b>Downloads</b> ${card.downloads}
+                </p>
+            </div>
+        </div>
+    `;
+};
+
+const generateGallery = (array) => {
+    array.forEach((card) => gallery.innerHTML += generateCard(card));
+};
+
+const checkIfEnd = (data) => {
+    const images = [...document.querySelectorAll('.photo-card')];
+    if (data.totalHits > data.hits.length) loadBtn.classList.toggle('hide');
+    if (images.length >= data.totalHits) {
+        loadBtn.classList.toggle('hide');
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    };
+};
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    page = 1;
+    gallery.innerHTML = '';
+    input = form.elements.searchQuery.value.trim();
+    loadBtn.classList.toggle('hide');
+
+    if (input) {
+        fetchImages(input, page).then(data => {
+            generateGallery(data.hits);
+            checkIfEnd(data);
+        });
+    } else {
+        Notiflix.Notify.failure("Input can't be empty");
+    };
+});
+
+loadBtn.addEventListener('click', () => {
+    page++;
+    fetchImages(input, page).then(data => {
+        generateGallery(data.hits);
+        checkIfEnd(data);
+    });
+});
